08ab3c66d922730b13fc776b86bc4a8c39d342c1
AbbeVass <134689455+AbbeVass@users.noreply.github.com>
2023-12-11 15:18:28 +0100

    files sorted

---
+++ script.js
@@ -0,0 +1,13 @@
+const amount = document.getElementById("amount");
+function buy() {
+    if (amount.value) {
+        var popup = document.createElement("div");
+        popup.className = "popup";
+        popup.innerHTML = "Din köp på " + amount.value + " kr är nu genomfört &#128077;";
+        const container = document.querySelector(".popup-container");
+        container.appendChild(popup);
+        setTimeout(function() {
+            container.removeChild(popup);
+        }, 2000);
+    }
+}
